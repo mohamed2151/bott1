4456ec43dba773f331621a2eb1685e02bf45e762
mohamed2151 <44812878+mohamed2151@users.noreply.github.com>
2018-12-02 11:59:48 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,7 @@
-
+client.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("518758073750519808");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`نورتنا`), 4000)        
+}
+});
